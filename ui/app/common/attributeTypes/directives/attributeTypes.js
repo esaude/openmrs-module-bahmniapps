7a2a22c500d7838b28f154c47d825a15d0e4a6f4
Edgêncio da Calista <edgencio@gmail.com>
2019-03-21 11:11:57 +0200

    Adds option to persist data using concepts

--- ui/app/common/attributeTypes/directives/attributeTypes.js
+++ ui/app/common/attributeTypes/directives/attributeTypes.js
@@ -15,22 +15,16 @@ angular.module('bahmni.common.attributeTypes', []).directive('attributeTypes', [
         },
         templateUrl: '../common/attributeTypes/views/attributeInformation.html',
         restrict: 'E',
-        controller: function ($scope, $log) {
+        controller: function ($scope) {
             $scope.getAutoCompleteList = $scope.getAutoCompleteList();
             $scope.getDataResults = $scope.getDataResults();
             // to avoid watchers in one way binding
-            $scope.isAutoComplete = $scope.isAutoComplete() || function () {
-                return false;
-            };
-            $scope.isReadOnly = $scope.isReadOnly() || function () {
-                return false;
-            };
-            $scope.handleUpdate = $scope.handleUpdate() || function () {
-                return false;
-            };
+            $scope.isAutoComplete = $scope.isAutoComplete() || function () { return false; };
+            $scope.isReadOnly = $scope.isReadOnly() || function () { return false; };
+            $scope.handleUpdate = $scope.handleUpdate() || function () { return false; };
 
-            //$scope.suggestions = $scope.attribute.answers;
-            $scope.suggestions = professions;
+            $scope.suggestions = $scope.attribute.answers;
+
             $scope.showTag = false;
 
             $scope.appendConceptNameToModel = function (attribute) {
@@ -41,26 +35,24 @@ angular.module('bahmni.common.attributeTypes', []).directive('attributeTypes', [
                 attributeValueConceptType.value = concept && concept.fullySpecifiedName;
             };
 
-
             $scope.suggest = function (string) {
-
                 $scope.hideList = false;
                 $scope.showTag = true;
                 var output = [];
                 angular.forEach($scope.suggestions, function (suggestion) {
-                    if (suggestion.name.toLowerCase().indexOf(string.toLowerCase()) >= 0) {
-                        output.push(suggestion.name);
+                    if (suggestion.description.toLowerCase().indexOf(string.value.toLowerCase()) >= 0) {
+                        output.push(suggestion);
                     }
                 });
                 $scope.filterOcuppation = output;
             };
 
-            $scope.hideSuggestions = function (string) {
-                $scope.targetModel[$scope.attribute.name] = string;
+            $scope.hideSuggestions = function (object) {
+                $scope.targetModel[$scope.attribute.name] = object;
+                $scope.targetModel[$scope.attribute.name].value = object.description;
+                $scope.targetModel[$scope.attribute.name].conceptUuid = object.conceptId;
                 $scope.hideList = true;
             };
-
-
         }
     };
 }]);
