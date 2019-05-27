d8842257b717a9363b353e0c07009657c4ee8741
Simao Mazive <simao.mazive@jembi.org>
2019-05-27 09:43:23 +0200

    MPOC-287 - BUG Fix
    The system automatically clear the HF code already set previously when any dropdown field is selected.

--- ui/app/common/attributeTypes/directives/attributeTypes.js
+++ ui/app/common/attributeTypes/directives/attributeTypes.js
@@ -56,7 +56,10 @@ angular.module('bahmni.common.attributeTypes', []).directive('attributeTypes', [
                     return answer.conceptId === attributeValueConceptType.conceptUuid;
                 });
                 attributeValueConceptType.value = concept && concept.fullySpecifiedName;
-                $rootScope.typeOfRegistrationSelected = attributeValueConceptType.value;
+                if (attributeValueConceptType.value === 'NEW_PATIENT' || attributeValueConceptType.value === 'TRANSFERRED_PATIENT' || attributeValueConceptType.value === undefined) {
+                    console.log(attributeValueConceptType.value);
+                    $rootScope.typeOfRegistrationSelected = attributeValueConceptType.value;
+                }
             };
 
             $scope.suggest = function (string) {
