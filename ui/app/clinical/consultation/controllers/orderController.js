fa11aa91c3af5aa0b90e2a2a423f2012f531c54a
Amol Chavan <amol@satvix.com>
2019-05-31 12:09:29 +0530

    Amol | Added Translation Urgent label from Order tab

--- ui/app/clinical/consultation/controllers/orderController.js
+++ ui/app/clinical/consultation/controllers/orderController.js
@@ -263,4 +263,8 @@ angular.module('bahmni.clinical')
             };
 
             init();
+
+            (function () {
+                $scope.clinicalOrderUrgent = {forwardUrl: Bahmni.Clinical.Constants.adtForwardUrl, title: $translate.instant("CLINICAL_ORDER_URGENT_TITLE"), privilege: Bahmni.Clinical.Constants.adtPrivilege };
+            })();
         }]);
