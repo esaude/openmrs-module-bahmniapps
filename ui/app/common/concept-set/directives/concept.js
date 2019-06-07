d433dd391a8e4f46710ad6bdc75d651eefbf7438
Sushant Patil <sushantpatil1214@gmail.com>
2019-06-07 15:11:08 +0530

    Sushant | MPOC-313 Include validation to calculate Estimated Delivery…

--- ui/app/common/concept-set/directives/concept.js
+++ ui/app/common/concept-set/directives/concept.js
@@ -68,54 +68,44 @@ angular.module('bahmni.common.conceptSet')
                 scope.$watch('collapseInnerSections', function () {
                     scope.collapse = scope.collapseInnerSections && scope.collapseInnerSections.value;
                 });
-
                 scope.handleUpdate = function () {
                     var currentEnteredDate;
-                     var currentValue;
-                    var date="";
-                     if (scope.observation.concept.name == 'Last Menstruation Date'){
+                    var currentValue;
+                    var date = "";
+                    if (scope.observation.concept.name == 'Last Menstruation Date') {
                         _.map(scope.rootObservation.groupMembers, function (currentObj) {
-                           
-                             if (currentObj.concept.name == 'Probable delivery date') { 
+                            if (currentObj.concept.name == 'Probable delivery date') {
                                 currentObj.value = date;
                                 return currentObj;
-                             } 
-                             if (currentObj.concept.name == 'Pregnancy_Yes_No') { 
+                            }
+                            if (currentObj.concept.name == 'Pregnancy_Yes_No') {
                                 currentObj.value = null;
                                 return currentObj;
-                             }    
-                            });
-                        }
+                            }
+                        });
+                    }
                     if (scope.observation.concept.name == 'Pregnancy_Yes_No') {
-                        
                         _.map(scope.rootObservation.groupMembers, function (currentObj) {
-            
-                            if (currentObj.concept.name == 'Last Menstruation Date'){
-
-                           currentEnteredDate=currentObj.value; 
-
+                            if (currentObj.concept.name == 'Last Menstruation Date') {
+                                currentEnteredDate = currentObj.value;
                             }
                             if (currentObj.concept.name == 'Probable delivery date') {
-                            
-                                    if(scope.observation.value === true) {
+                                if (scope.observation.value === true) {
                                     currentEnteredDate = moment(currentEnteredDate).add(9, 'M');
                                     currentEnteredDate = moment(currentEnteredDate).add(7, 'days');
                                     currentEnteredDate = moment(currentEnteredDate).format('YYYY-MM-DD');
                                     currentObj.value = currentEnteredDate;
-                                }
-                                else{
+                                } else {
                                     currentObj.value = date;
                                 }
-                            
+
                                 return currentObj;
                             }
                             return currentObj;
-                        
                         });
                     }
                     scope.$root.$broadcast("event:observationUpdated-" + scope.conceptSetName, scope.observation.concept.name, scope.rootObservation);
                 };
-
                 scope.update = function (value) {
                     if (scope.getBooleanResult(scope.observation.isObservationNode)) {
                         scope.observation.primaryObs.value = value;
