d861fc91a23e7be4ed80012312fecd36f4123412
Edgêncio da Calista <edgencio@gmail.com>
2019-06-11 11:29:08 +0200

    Adds improvements on validations

--- ui/app/appointments/controllers/manage/appointmentsCreateController.js
+++ ui/app/appointments/controllers/manage/appointmentsCreateController.js
@@ -66,6 +66,8 @@ angular.module('bahmni.appointments')
                 } else if (!moment($scope.appointment.startTime, 'hh:mm a')
                     .isBefore(moment($scope.appointment.endTime, 'hh:mm a'), 'minutes')) {
                     message = 'TIME_SEQUENCE_ERROR_MESSAGE';
+                } else if ($scope.invalidChosenDate) {
+                    message = 'APPOINTMENT_INVALID_DATE';
                 }
                 if (message) {
                     messagingService.showMessage('error', message);
@@ -102,6 +104,8 @@ angular.module('bahmni.appointments')
                 } else if (!moment($scope.appointment.startTime, 'hh:mm a')
                     .isBefore(moment($scope.appointment.endTime, 'hh:mm a'), 'minutes')) {
                     message = 'TIME_SEQUENCE_ERROR_MESSAGE';
+                } else if ($scope.invalidChosenDate) {
+                    message = 'APPOINTMENT_INVALID_DATE';
                 }
                 if (message) {
                     messagingService.showMessage('error', message);
@@ -568,14 +572,16 @@ angular.module('bahmni.appointments')
             };
 
             $scope.validateDate = function () {
-                var choosenDate = Bahmni.Common.Util.DateUtil.getDateWithoutTime($scope.appointment.date);
+                var chosenDate = Bahmni.Common.Util.DateUtil.getDateWithoutTime($scope.appointment.date);
 
                 var today = new Date($scope.today);
-                var selectedDate = new Date(choosenDate);
-                if (selectedDate.getTime() < today.getTime()) {
+                var selectedDate = new Date(chosenDate);
+                if ((chosenDate != null && selectedDate.getTime() < today.getTime()) || selectedDate.getUTCFullYear() === 1970) {
+                    $scope.invalidChosenDate = true;
                     angular.element("#date").css("border", "2px solid #ff3434");
                     angular.element("#date").css("background", "#ffcdcd");
                 } else {
+                    $scope.invalidChosenDate = false;
                     angular.element("#date").css("border", "1px solid #d1d1d1");
                     angular.element("#date").css("background", "#fff");
                 }
