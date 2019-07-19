d6b26259a1a8643a4e14f0cb473c9f07abc41975
= <ekirapapaul@gmail.com>
2019-07-19 18:00:40 +0300

    MPOC-478 fix bug for null patient on appointment edit

--- ui/app/appointments/controllers/manage/appointmentsCreateController.js
+++ ui/app/appointments/controllers/manage/appointmentsCreateController.js
@@ -53,7 +53,9 @@ angular.module('bahmni.appointments')
                 $scope.appointment = Bahmni.Appointments.AppointmentViewModel.create(appointmentContext.appointment || { appointmentKind: 'Scheduled' }, appointmentCreateConfig);
                 $scope.selectedService = appointmentCreateConfig.selectedService;
                 $scope.isPastAppointment = $scope.isEditMode() ? Bahmni.Common.Util.DateUtil.isBeforeDate($scope.appointment.date, moment().startOf('day')) : false;
-                $scope.appointment.patient = $state.params.patient;
+                if ($state.params.patient) {
+                    $scope.appointment.patient = $state.params.patient;
+                }
                 $scope.appointment.date = $stateParams.selectedAppointmentDate || null;
                 $scope.selectedAppointmentBlock = $stateParams.selectedAppointmentBlock;
                 if ($scope.selectedAppointmentBlock) {
