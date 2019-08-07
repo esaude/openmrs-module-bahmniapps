62b959790a3d2ec5f4669b1197b21c4f8e9b1e20
satvix <pratikthaakare@gmail.com>
2019-08-07 17:30:03 +0530

    Pratik | Fixed Print prescription dropdown button

--- ui/app/common/ui-helper/directives/splitButtonNew.js
+++ ui/app/common/ui-helper/directives/splitButtonNew.js
@@ -29,7 +29,7 @@ angular.module('bahmni.common.uiHelper')
         return {
             restrict: 'A',
             template: '<div class="split-button" bm-pop-over>' +
-                '<button bm-pop-over-trigger class="toggle-buttonNew fa fa-caret-down" ng-show="::hasMultipleOptions()" ng-click="scrollToBottom()" ng-disabled="optionDisabled" type="button style="color:"black"></button>' +
+                '<button bm-pop-over-trigger style="height:31px;width:40px" class="toggle-buttonNew fa fa-caret-down" ng-show="::hasMultipleOptions()" ng-click="scrollToBottom()" ng-disabled="optionDisabled" type="button style="color:"black"></button>' +
                 '<ul class="options">' +
                 '<li>' +
                 '<button class="buttonClass" ng-click="optionClick()(primaryOption)" accesskey="{{::primaryOption.shortcutKey}}" ng-disabled="optionDisabled" ng-bind-html="::optionText()(primaryOption,\'primary\') | translate "></button>' +
