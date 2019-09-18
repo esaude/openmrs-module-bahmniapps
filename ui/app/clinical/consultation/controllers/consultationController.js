0a07f6cfd98a93606c4e8adba0edabc82374a599
Nathan Floor <nathan.floor@jembi.org>
2019-09-18 10:13:31 +0200

    Initial commit for implementing fix for clinical forms not appearing when there are many providers (#361)
    * Initial commit for implementing fix for clinical forms not appearing when there are many providers
    
    * Fixed linting issues
    
    * Fixed the undefined issue and added some validations

--- ui/app/clinical/consultation/controllers/consultationController.js
+++ ui/app/clinical/consultation/controllers/consultationController.js
@@ -5,11 +5,11 @@ angular.module('bahmni.clinical').controller('ConsultationController',
         'spinner', 'encounterService', 'messagingService', 'sessionService', 'retrospectiveEntryService', 'patientContext', '$q',
         'patientVisitHistoryService', '$stateParams', '$window', 'visitHistory', 'clinicalDashboardConfig', 'appService',
 
-        'ngDialog', '$filter', 'configurations', 'visitConfig', 'conditionsService', 'configurationService', 'auditLogService', 'allergiesService', 'printer', 'printPrescriptionReportService', 'providerTypeService', '$http', 'patientService', 'transferOutService',
+        'ngDialog', '$filter', 'configurations', 'visitConfig', 'conditionsService', 'configurationService', 'auditLogService', 'allergiesService', 'printer', 'printPrescriptionReportService', 'providerService', 'providerTypeService', '$http', 'patientService', 'transferOutService',
         function ($scope, $rootScope, $state, $location, $translate, clinicalAppConfigService, diagnosisService, urlHelper, contextChangeHandler,
             spinner, encounterService, messagingService, sessionService, retrospectiveEntryService, patientContext, $q,
             patientVisitHistoryService, $stateParams, $window, visitHistory, clinicalDashboardConfig, appService,
-            ngDialog, $filter, configurations, visitConfig, conditionsService, configurationService, auditLogService, allergiesService, printer, printPrescriptionReportService, providerTypeService, $http, patientService, transferOutService) {
+            ngDialog, $filter, configurations, visitConfig, conditionsService, configurationService, auditLogService, allergiesService, printer, printPrescriptionReportService, providerService, providerTypeService, $http, patientService, transferOutService) {
             var DateUtil = Bahmni.Common.Util.DateUtil;
             var getPreviousActiveCondition = Bahmni.Common.Domain.Conditions.getPreviousActiveCondition;
             var currentProviderType;
@@ -205,10 +205,12 @@ angular.module('bahmni.clinical').controller('ConsultationController',
                 var adtNavigationConfig = appService.getAppDescriptor().getConfigValue('adtNavigationConfig');
                 Object.assign($scope.adtNavigationConfig, adtNavigationConfig);
                 setCurrentBoardBasedOnPath();
-                return $q.all([providerTypeService.getAllProviders()]).then(function (results) {
-                    var allProviders = results[0];
-                    var currentProvider = $rootScope.currentProvider;
-                    currentProviderType = _.filter(providerTypeService.getProviderType(allProviders, currentProvider)[0])[0];
+                var currentProvider = $rootScope.currentProvider;
+                return $q.all([providerService.getProviderAttributes(currentProvider.uuid)]).then(function (response) {
+                    if (response[0].data) {
+                        var providerAttributes = response[0].data.results;
+                        currentProviderType = providerTypeService.getProviderType(providerAttributes)[0];
+                    }
                 });
             };
 
