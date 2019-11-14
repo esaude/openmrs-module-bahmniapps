34155adb76f753c0ed91ad6db36f7e9ff125529c
Simao Mazive <simao.mazive@jembi.org>
2019-11-14 10:35:28 +0200

    Mandatory Field Restriction for Transferring In existing Patients

--- ui/app/registration/directives/topDownAddressFields.js
+++ ui/app/registration/directives/topDownAddressFields.js
@@ -182,7 +182,9 @@ angular.module('bahmni.registration')
             var deregisterAddressWatchForPatientState = $scope.$watch('patient', function (newValue) {
                 if (newValue !== undefined) {
                     if (newValue == "INACTIVE_SUSPENDED" || newValue === "INACTIVE_TRANSFERRED_OUT" || newValue === "INACTIVE_DEATH") {
-                        $scope.isAddressDisabled = true;
+                        if (document.getElementById("cityVillage")) {
+                            $scope.isAddressDisabled = true;
+                        }
                     } else {
                         $scope.isAddressDisabled = false;
                     }
