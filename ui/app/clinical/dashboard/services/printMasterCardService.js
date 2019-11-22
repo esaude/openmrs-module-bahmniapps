510cb78d16a5562732730b993ef435b227ad54b4
Simao Mazive <simao.mazive@jembi.org>
2019-11-22 10:49:25 +0200

    Conflict fix

--- ui/app/clinical/dashboard/services/printMasterCardService.js
+++ ui/app/clinical/dashboard/services/printMasterCardService.js
@@ -133,7 +133,7 @@ angular.module('bahmni.clinical')
                     var p20 = populatePsychosocialFactors();
                     var p25 = dispenseddrug();
 
-                    Promise.all([p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p25]).then(function () {
+                    Promise.all([p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25]).then(function () {
                         resolve(masterCardModel);
                     }).catch(function (error) {
                         reject(error);
